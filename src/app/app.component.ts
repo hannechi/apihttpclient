9aa4a3f31303a1595619dca31425337d053f4978
hannechi <hannechichiheb@gmail.com>
2023-08-02 16:01:40 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'httpclientprojectfakeapi';
+  post:any;
+  tableauposts : any[]=[];
+  constructor(private service : ApiService)
+  {
+
+  }
+
+  ngOnInit(): void {
+     var resp = this.service.getPosts();
+     resp.subscribe(
+      {
+        next: (data)=>
+        {
+            this.tableauposts=data        },
+        error: (errr)=>
+        {
+
+        }
+      }
+    )
+    }
+    getdata(text:any)
+    {
+          this.service.getPostbyid(text.value).subscribe(
+            {
+              next: (resp)=>
+              {
+                this.post=resp;
+              },
+              error:(err)=>
+              {
+                console.log(err)
+              }
+            }
+          )
+    }
+
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http : HttpClient) { }
+
+  getPosts():Observable<any>
+  {
+    return this.http.get("https://jsonplaceholder.typicode.com/posts");
+  }
+
+
+  getPostbyid(id:any):Observable<any>
+  {
+    return this.http.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  }
+
+}
